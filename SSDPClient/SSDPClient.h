2f1806bed7a352c1d13edc6b393549106e8c4be8
Pierrick Rouxel <pierrick.rouxel@me.com>
2015-03-21 10:35:49 +0100

    Initial Commit

---
+++ SSDPClient/SSDPClient.h
@@ -0,0 +1,19 @@
+//
+//  SSDPClient.h
+//  SSDPClient
+//
+//  Created by Pierrick Rouxel on 21/03/2015.
+//  Copyright (c) 2015 Pierrick Rouxel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SSDPClient.
+FOUNDATION_EXPORT double SSDPClientVersionNumber;
+
+//! Project version string for SSDPClient.
+FOUNDATION_EXPORT const unsigned char SSDPClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSDPClient/PublicHeader.h>
+
+
