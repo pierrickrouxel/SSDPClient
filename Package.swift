bd13cffddbc71ecaa35c32ba994561a4f50d7430
Pierrick Rouxel <p.rouxel@orisha.com>
2024-07-29 14:58:21 +0200

    feat: update swiftpm

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 
 import PackageDescription
 
@@ -8,15 +8,15 @@ let package = Package(
         .library(name: "SSDPClient", targets: ["SSDPClient"]),
     ],
     dependencies: [
-        .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket.git", from: "1.0.200"),
+        .package(url: "https://github.com/Kitura/BlueSocket.git", from: "1.0.200"),
         .package(url: "https://github.com/Kitura/HeliumLogger.git", from: "1.9.200"),
     ],
     targets: [
         .target(
             name: "SSDPClient",
             dependencies: [
-                "Socket",
-                "HeliumLogger",
+                .product(name: "Socket", package: "BlueSocket"),
+                .product(name: "HeliumLogger", package: "HeliumLogger")
             ]
         ),
         .testTarget(
